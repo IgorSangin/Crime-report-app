49160697b0e77ec27c525a44aaa9f6b8acc7f4d3
sangini <sangini@uni.coventry.ac.uk>
2020-04-06 20:51:41 +0100

    Report page in progress

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -2,11 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Home extends AppCompatActivity {
+
+    Button btnReport;
 
     FirebaseDatabase database;
 
@@ -14,6 +19,15 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnReport = (Button) findViewById(R.id.btnReport);
+        btnReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent report = new Intent(Home.this, Report.class);
+                startActivity(report);
+            }
+        });
 
         database = FirebaseDatabase.getInstance();
     }
