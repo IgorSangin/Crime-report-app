2fd528a5441c8f3d37ed20601c45631c7817df7e
sangini <sangini@uni.coventry.ac.uk>
2020-04-07 15:15:01 +0100

    Basics of report page done

--- app/src/main/java/com/example/myapplication/Report.java
+++ app/src/main/java/com/example/myapplication/Report.java
@@ -2,13 +2,45 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Report extends AppCompatActivity {
+public class Report extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    EditText location, textArea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        Spinner spinner = findViewById(R.id.spinnerMain);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.options, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        location = findViewById(R.id.location);
+        textArea = findViewById(R.id.textArea);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
